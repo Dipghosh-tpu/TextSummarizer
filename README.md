500a82fbe1e233e48289764518eff2537cc2beaa
Sunanda Kumar Ghosh <dipghosh519@gmail.com>
2025-04-17 13:42:50 +0530

    Completed Data Ingestion notebook and added data_ingestion.py

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 1. config.yaml
 2. params.yaml
 3. Config Entity
-4. Configuration Manager
+4. Configuration Manager -> Read yaml of config.yaml, params.yaml and DataIngestionConfig (dataClass)
 5. Update the Components - Data Ingestion, Data Transformation, Model Trainer
 6. Create the Pipelines  - Trainer Pipeline, Prediction Pipeline
 7. Front End - API's, Training API's, Batch Prediction API's.
---
+++ src/textSummarizer/components/data_ingestion.py
@@ -0,0 +1,32 @@
+import os
+import urllib.request as request
+import py7zr
+from textSummarizer.logging import logger
+from textSummarizer.utils.common import get_size
+from pathlib import Path
+from textSummarizer.entity import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url=self.config.source_URL,
+                filename=self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with following info: \n{headers}")
+        else:
+            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")
+
+    def extract_zip_file(self):
+        """
+        Extracts the 7z file using py7zr library
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        
+        with py7zr.SevenZipFile(self.config.local_data_file, mode='r') as z:
+            z.extractall(path=unzip_path)
+            logger.info(f"7z file extracted to: {unzip_path}")
