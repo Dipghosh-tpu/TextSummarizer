8e34565f44fb1aa195e3a15b41fdf046ef731634
Sunanda Kumar Ghosh <dipghosh519@gmail.com>
2025-04-18 13:07:08 +0530

    Fix: Completed Configuration Manager and Data Ingestion completed

--- src/textSummarizer/components/data_ingestion.py
+++ src/textSummarizer/components/data_ingestion.py
@@ -1,10 +1,10 @@
 import os
 import urllib.request as request
 import py7zr
-from textSummarizer.logging import logger
-from textSummarizer.utils.common import get_size
+from src.textSummarizer.logging import logger
+# from src.textSummarizer.utils.common import get_size
 from pathlib import Path
-from textSummarizer.entity import DataIngestionConfig
+from src.textSummarizer.entity import DataIngestionConfig
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
